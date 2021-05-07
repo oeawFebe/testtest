022074f69d124edf9d340a17c6688063979a3101
oeawfebe <oeawfebe@tutanota.com>
2021-05-06 21:10:47 -0400

    added aftersetup.bat

---
+++ aftersetup.bat
@@ -0,0 +1,5 @@
+@ECHO
+:: This batch file pip installs fabric3 and runs C:\Users\Owner\Desktop\django_eth\django_eth\poll_get_data_route.py
+cd C:\Users\Owner\Desktop\django_eth\django_eth
+C:\Users\Owner\AppData\Local\Programs\Python\Python39\Scripts\pip.exe install fabric3
+C:\Users\Owner\AppData\Local\Programs\Python\Python39\python.exe poll_get_data_route.py
